bbfa6cc86ba7b5782a58e97651318b4e70681e56
maxprotasov <mo2k4k@yandex.ru>
2017-07-11 09:52:00 +0300

    add brain-even

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-import readlineSync from 'readline-sync';
 
+import readlineSync from 'readline-sync';
 
 const welcome = () => {
   console.log('Welcome to the Brain games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello ${name}`);
-}
+  const getRandomInt = () => Math.floor(Math.random() * 100);
+  let i = 3;
+
+  while (i) {
+    const int = getRandomInt();
+    console.log(int);
+    const numLog = readlineSync.question('Your answer:');
+
+    if (int % 2 === 0 && numLog === 'yes') {
+      console.log('Correct!');
+      i--;
+    } else if (int % 2 !== 0 && numLog === 'no') {
+      console.log('Correct!');
+      i--;
+    } else {
+      console.log(`Let's try again, ${name}!`);
+      i = 3;
+    }
+  }
+  return console.log(`Congratulations, ${name}!`);
+};
+
 export default welcome;
